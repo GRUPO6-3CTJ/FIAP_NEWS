cf6698d4e43182a27c492f15637aad865757d287
GRUPO6-3CTJ <3ctjgrupo6@gmail.com>
2025-02-25 21:59:49 -0300

    Add files via upload

--- backend/models/news.js
+++ backend/models/news.js
@@ -1,11 +1,28 @@
 const mongoose = require('mongoose');
 
+// Definir um esquema para as notícias
 const newsSchema = new mongoose.Schema({
-    title: { type: String, required: true },
-    content: { type: String, required: true },
-    author: { type: String, required: true },
-    category: { type: String, required: true },
-    date: { type: Date, default: Date.now }
+    title: {
+        type: String,
+        required: true // O título é obrigatório
+    },
+    content: {
+        type: String,
+        required: true // O conteúdo é obrigatório
+    },
+    author: {
+        type: String,
+        required: true // O autor é obrigatório
+    },
+    category: {
+        type: String,
+        required: true // A categoria é obrigatória
+    },
+    date: {
+        type: Date,
+        default: Date.now // A data atual será atribuída por padrão
+    }
 });
 
+// Exportar o modelo baseado no esquema
 module.exports = mongoose.model('News', newsSchema);
